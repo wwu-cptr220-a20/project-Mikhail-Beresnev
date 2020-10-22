3a7686cb17b00e7336837c4ff148d21bfba73605
Mikhail Beresnev <MBeresnev01@gmail.com>
2020-10-21 22:24:18 -0400

    Finish Stage 3

---
+++ project/chatroom/chatroom.js
@@ -0,0 +1,12 @@
+/*
+
+Use the object to load the conversation based on the code
+
+*/
+
+codeMap[chatroomCode]['chat'].forEach(element => {
+    // print out the conversation
+    // element.key
+    // element.value
+});
+
